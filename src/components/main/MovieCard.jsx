b00010bb9e4473e875a18964ee29f8f54ecdd761
kevinsauvage <kevinsauvage@outlook.com>
2020-12-12 19:28:28 +0100

    adding animation to card

--- src/components/main/MovieCard.jsx
+++ src/components/main/MovieCard.jsx
@@ -3,7 +3,6 @@ import "react-multi-carousel/lib/styles.css";
 
 const MovieCard = ({ movie, handleCardClickShow }) => {
   let backgroundImage = `url(https://image.tmdb.org/t/p/w342/${movie.poster_path})`;
-
   return (
     <div
       key={movie.id}
@@ -17,8 +16,30 @@ const MovieCard = ({ movie, handleCardClickShow }) => {
         position: "relative",
         height: "100%",
       }}>
-      <div className="title">
-        <h3>{movie.original_title}</h3>
+      <div
+        data-key={movie.id}
+        onClick={(e) => {
+          handleCardClickShow(e);
+        }}
+        className="detail-card-hover">
+        <div>
+          <p
+            data-key={movie.id}
+            onClick={(e) => {
+              handleCardClickShow(e);
+            }}
+            className="title-card-hover">
+            {movie.title}
+          </p>
+          <p
+            data-key={movie.id}
+            onClick={(e) => {
+              handleCardClickShow(e);
+            }}>
+            {movie.release_date}
+          </p>
+        </div>
+        <div className="vote-card-detail">{movie.vote_average}</div>
       </div>
     </div>
   );
