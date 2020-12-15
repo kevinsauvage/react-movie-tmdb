df7d98944ef599ce02d6c87e74ac377952d044fd
kevinsauvage <kevinsauvage@outlook.com>
2020-12-15 22:12:33 +0100

    changing name youtube component

--- src/components/main/CardDetail.jsx
+++ src/components/main/CardDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaStar, FaTimesCircle } from "react-icons/fa";
-import YoutubePlayer from "../main/YoutubePlayer";
+import YoutubePlayer from "../main/YoutubeVideoPlayer";
 
 const CardDetail = ({
   singleMovie,
