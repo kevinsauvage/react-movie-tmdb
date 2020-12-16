aec2289aaed2825c9b1d4c666331aeaf084ccc11
kevinsauvage <kevinsauvage@outlook.com>
2020-12-16 10:38:19 +0100

    adding condition to display the videoplayer, if no trailer was found, display 'no trailer found'

--- src/components/main/YoutubeVideoPlayer.jsx
+++ src/components/main/YoutubeVideoPlayer.jsx
@@ -1,25 +1,44 @@
 import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player/youtube";
-import { FaTimesCircle } from "react-icons/fa";
+import { FaSadTear, FaTimesCircle } from "react-icons/fa";
 
 const YoutubePlayer = ({ singleMovie, SetVideoIsOpen }) => {
-  return (
-    <div className="video-player-container">
-      <ReactPlayer
-        url={
-          "https://www.youtube.com/watch?v=" + singleMovie.videos.results[0].key
-        }
-        controls={true}
-        width="100%"
-        height="100%"
-      />
-      <div className="icon-wrapper">
+  if (singleMovie.videos.results.length !== 0) {
+    return (
+      <div className="video-player-container">
+        <ReactPlayer
+          url={
+            "https://www.youtube.com/watch?v=" +
+            singleMovie.videos.results[0].key
+          }
+          controls={true}
+          width="100%"
+          height="100%"
+        />
+        <div className="icon-wrapper">
+          <div
+            className="icon-close-video"
+            onClick={() => SetVideoIsOpen(false)}>
+            <FaTimesCircle size="30px" style={{ cursor: "pointer" }} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="no-trailer-container">
+        <div className="sad-icon-container">
+          <FaSadTear size="50px" />
+        </div>
         <div className="icon-close-video" onClick={() => SetVideoIsOpen(false)}>
           <FaTimesCircle size="30px" style={{ cursor: "pointer" }} />
         </div>
+        <div className="no-trailer-text-container">
+          <p>Sorry, no trailer was found...</p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default YoutubePlayer;
