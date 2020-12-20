41339b46533db5b830d47bbe5f1098427c266786
kevinsauvage <kevinsauvage@outlook.com>
2020-12-20 21:11:06 +0100

    changin background color card detail

--- src/components/main/Content.jsx
+++ src/components/main/Content.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useEffect, useContext } from "react";
 import Carusel from "../main/Carusel";
 import SectionTitle from "../main/SectionTitle";
 import { AppContext } from "../../Context/AppContext";
 
 const Content = () => {
-  const API_KEY = process.env.REACT_APP_API_KEY;
-
   const props = useContext(AppContext);
 
   useEffect(() => {
