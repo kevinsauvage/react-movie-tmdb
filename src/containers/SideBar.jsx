088ce033c41d98ad98d34b31b076ae84aecc965b
kevin sauvage <kevinsauvage@outlook.com>
2021-02-23 15:50:35 +0100

    handling click ouside menu small screen

--- src/containers/SideBar.jsx
+++ src/containers/SideBar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useRef } from "react";
 import Library from "../components/sidebar/Library";
 import Logo from "../components/sidebar/Logo";
 import SortByNav from "../components/sidebar/SortByNav";
@@ -6,15 +7,25 @@ import { AppContext } from "../Context/AppContext";
 
 const SideBar = () => {
   const props = useContext(AppContext);
+  const Sidebar = useRef(null);
 
   const styleOpen = {
-    left: "0",
+    transform: "translateX(100vw)",
     opacity: "1",
-    transition: "left 1s ease",
+    transition: "transform 1s ease",
+  };
+  const handleClick = (e) => {
+    if (Sidebar.current && Sidebar.current.contains(e.target)) {
+      props.setOpenMenuHamb(false);
+    }
   };
 
   return (
-    <div className="side-bar" style={props.openMenuHamb ? styleOpen : null}>
+    <div
+      ref={Sidebar}
+      onClick={handleClick}
+      className="side-bar"
+      style={props.openMenuHamb ? styleOpen : null}>
       <Logo className="logo" />
       <Library />
       <SortByNav />
